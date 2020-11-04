e178d293be2de07e4e9de25aef11b2f59ee69155
RobColeman98 <Robcoleman98@gmail.com>
2020-11-04 10:58:13 -0600

    first commit

--- app.ts
+++ app.ts
@@ -1,43 +1,46 @@
-$(document).ready(function () {
-  let $container = $('<div class="container"></div>');
+$(document).on('ready', function () {
+  let $container: JQuery<HTMLDivElement> = $('<div class="container"></div>');
   $("body").append($container);
   let sqrId = 1;
   let diceArr = [];
-  $("#button").click(() => {
+  $("#button").on("click", () => {
     let newDie = new Die(Math.floor(Math.random() * 6) + 1);
     diceArr.push(newDie);
   });
-  $(".button2").click(() => {
+  $(".button2").on("click", () => {
     diceArr.forEach((die) => die.roll());
   });
-  $(".button3").click(() => {
+  $(".button3").on("click", () => {
     let sum = 0;
     diceArr.forEach((die) => (sum += die.value));
     alert(sum);
   });
 
   class Die {
-    constructor(value) {
+      value: number;
+      square: JQuery<HTMLDivElement>;
+    
+    constructor(value: any) {
       this.value = value;
       this.square = $('<div class="square"></div>');
       $container.append(this.square);
-      this.square.id = sqrId;
+      this.square.attr("id", sqrId);
       this.square.text(this.value);
       sqrId++;
-      this.square.click(() => this.roll());
-      
-      this.square.dblclick(() => {
-        if (this.square.value % 1 == 0) {
-          if (square.nextSibling) {
-            $container.removeChild(square.nextSibling);
+      this.square.on("click", () => this.roll());
+
+      this.square.on('dblclick', () => {
+        if (this.value % 1 == 0) {
+          if (this.square.next()) {
+            $container.find(this.square.next().remove())
           } else {
             alert("No die here man!");
           }
         }
 
-        if (this.square.value % 0 == 0) {
-          if (square.nextSibling) {
-            $container.removeChild(square.nextSibling);
+        if (this.value % 0 == 0) {
+          if (this.square) {
+            $container.find(this.square.prev().remove());
           } else {
             alert("No die here either man!");
           }
